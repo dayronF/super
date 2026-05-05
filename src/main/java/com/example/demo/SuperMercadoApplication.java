c08bb9fcf3a7085d838b783176151f6a7861e594
dayronF <dayronflorez0@gmail.com>
2026-05-05 09:28:13 -0500

    feat: Creacion de proyecto inicial

---
+++ src/main/java/com/example/demo/SuperMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMercadoApplication.class, args);
+	}
+
+}
