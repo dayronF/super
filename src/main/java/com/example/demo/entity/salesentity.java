7ce5fa81960db99b09700c622255c60cf9500f95
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-05-08 11:12:29 -0500

    feat: Creación de la entidad de ventas

---
+++ src/main/java/com/example/demo/entity/salesentity.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "sales")
+public class salesentity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cashier_id")
+    private int cashier_id;
+
+    @Column(name = "total",precision = 10, scale = 2)
+    private double total;
+
+    @Column(name = "created_at",nullable = false)
+    private LocalDateTime created_at;
+}
