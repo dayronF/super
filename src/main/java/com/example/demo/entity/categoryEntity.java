3a0efb76a5d0788490995dad0d219bf09d403b02
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-05-08 20:31:23 -0500

    feat: Se crea el repositorio de las categorias

--- src/main/java/com/example/demo/entity/categoryEntity.java
+++ src/main/java/com/example/demo/entity/categoryEntity.java
@@ -11,7 +11,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "categories")
-public class categoryEntity {
+public class CategoryEntity {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public class CategoryRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.CategoryEntity;
+
+public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
+    Optional<CategoryEntity> findByName(String name);
 }
