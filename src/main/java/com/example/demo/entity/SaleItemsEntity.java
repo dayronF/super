b45487513c04d2ac36a89c3ee1212de93422024e
dayronF <dayronflorez0@gmail.com>
2026-05-08 19:11:46 -0500

    fix: Correcion de clases

--- src/main/java/com/example/demo/entity/SaleItemsEntity.java
+++ src/main/java/com/example/demo/entity/SaleItemsEntity.java
@@ -12,7 +12,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "sale_items")
-public class SaleItems {
+public class SaleItemsEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/example/demo/entity/salesentity.java
+++ src/main/java/com/example/demo/entity/salesentity.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "sales")
-public class salesentity {
+public class SalesEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
