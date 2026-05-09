1efa7035819769558b35ec81a18a9100cdab7493
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-05-08 20:28:40 -0500

    feat: Se crea el repositorio del usuario

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.example.demo.repository;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
