51d6c93001d4a1d77cf28c0756cce380750dd5c5
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-05-09 22:31:22 -0500

    feat: Se crea los dtos de la entidad SaleItem

--- src/main/java/com/example/demo/dto/sale/SaleRequest.java
+++ src/main/java/com/example/demo/dto/sale/SaleRequest.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 import com.example.demo.dto.SaleItem.SaleItemRequest;
 
+import lombok.Data;
+
+@Data
 public class SaleRequest {
     private Integer cashierId;
     private List<SaleItemRequest> items;
