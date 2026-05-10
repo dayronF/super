f86195b7b488b53df6c5bc781233cab25f12c775
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-05-09 22:14:56 -0500

    feat: Se crea los dtos de la entidad producto

---
+++ src/main/java/com/example/demo/dto/Product/ProductResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto.Product;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Datagit 
+public class ProductResponse {
+    private Integer id;
+    private String name;
+    private BigDecimal price;
+    private Integer stock;
+    private String categoryName;
+    private String supplierName;
+    private LocalDateTime createdAt;
+}
