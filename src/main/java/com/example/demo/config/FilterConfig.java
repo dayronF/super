735ec7f2eeec74ff8a7a2c2245075984273f45bf
dayronF <dayronflorez0@gmail.com>
2026-05-10 21:48:37 -0500

    feat:Configuracion base del jwt

---
+++ src/main/java/com/example/demo/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.filter.JwtValidationFilter;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    FilterRegistrationBean<JwtValidationFilter> jwtFilter(JwtValidationFilter jwtValidationFilter) {
+
+        FilterRegistrationBean<JwtValidationFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(jwtValidationFilter);
+
+        registrationBean.addUrlPatterns("/*");
+
+        registrationBean.setOrder(0);
+
+        return registrationBean;
+    }
+}
